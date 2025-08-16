fe88b612c67a16eb519c9232b145a7c75bc8a43f
Acomage <acomage468@gmail.com>
2025-08-16 17:48:01 +0800

    fix typo

--- lib/config.py
+++ lib/config.py
@@ -5,15 +5,19 @@ CONFIG = {
 }
 
 # example for beta and nightly channels
-# CONIFG = {
+# CONFIG = {
 #     "channel": "beta",
 #     "version": "4.23.0-rc2",
 #     "created_at": "2025-08-15T00:56:31Z",
 # }
 # CONFIG = {
 #     "channel": "nightly",
-#     "version": "4.23.0",
-#     "created_at": "2025-08-14T02:53:01Z",
+#     "version": "4.24.0",
+#     "created_at": "2025-08-16T05:30:36Z",
 # }
 
 DATA_PATH = "/home/acomage/下载/lean-4.22.0-linux.tar.zst"
+
+DATA_PATH = "/home/acomage/下载/lean-4.23.0-rc2-linux.tar.zst"
+
+DATA_PATH = "/home/acomage/下载/lean-4.24.0-nightly-2025-08-16-linux.tar.zst"
